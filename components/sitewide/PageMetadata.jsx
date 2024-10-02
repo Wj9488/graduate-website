70889bd2ac0d2ac5aad435c830471a816d467225
Will Jones <wtjones10@gmail.com>
2024-10-02 10:53:50 +0100

    testing Og:image

--- components/sitewide/PageMetadata.jsx
+++ components/sitewide/PageMetadata.jsx
@@ -4,15 +4,18 @@ const PageMetadata = ({ PageTitle }) => {
   return (
     <Head>
     <title>{PageTitle} | William Jones</title>
-    <link rel="shortcut icon" href="icon.png" />
+    <link rel="shortcut icon" href="/icon.png" />
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:locale" content="en_GB" />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content="William Jones" />
-    <meta property="og:description" content="My personal website - find out more about me." />
-    <meta name="description" content="My personal website - find out more about me." />
-    <meta property="og:image" content="og-image-default.png" />
+    <meta property="og:description" content="I'm a recent business management graduate from Exeter University holding a First Class degree in Business Management.
+    I'm a bilingual English / French national. Tap the link to find out more about me. " />
+    <meta name="description" content="I'm a recent business management graduate from Exeter University holding a First Class degree in Business Management.
+    I'm a bilingual English / French national. Tap the link to find out more about me. 
+    " />
+    <meta property="og:image" content="/og-image-default.png" />
     <meta property="og:title" content={`${PageTitle} | William Jones`}/>
     <meta name="keywords" content="" />
   </Head>
