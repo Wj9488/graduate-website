c77dce32f1426fa2e1269d44052d854dba3b9183
Will Jones <wtjones10@gmail.com>
2024-10-07 17:32:21 +0100

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    siteUrl: 'https://william-jones.co',
+    generateRobotsTxt: true, // generates robots.txt
+    // Optional configuration
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    exclude: ['/sample-page', '/admin/*'], // exclusion pages
+    alternateRefs: [
+    ],
+  };
+  
