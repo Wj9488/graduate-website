fb16ce9780aa5f8c55dbd4273617e84c1df3d769
Will Jones <wtjones10@gmail.com>
2024-10-20 23:07:54 +0100

    changed button colours

--- components/sitewide/AlternativeMenu.jsx
+++ components/sitewide/AlternativeMenu.jsx
@@ -175,10 +175,22 @@ const AlternativeMenu = () => {
               id="DesktopOnly"
             >
               <Link className="" href={"/contact"}>
-                <div className="hover:opacity-75 transition-all duration-200 px-4 py-2 accent__c_main rounded-lg">
-                  <p className="2xl:text-base text-sm button__text_colour">
+                <div className="flex items-center gap-2 hover:opacity-75 transition-all duration-200 px-4 py-2 accent__c_main rounded-lg">
+                  <p className="2xl:text-base text-sm button__text_colour font-medium">
                     Contact Me
                   </p>
+                  <svg
+                      className="w-[17px] h-[17px] fill-[#2a2a2a]"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
                 </div>
               </Link>
             </a.button>
@@ -270,7 +282,7 @@ const AlternativeMenu = () => {
                   <Link
                     onClick={toggleMenu}
                     href={item.path}
-                    className="transition-all duration-200 ease-in-out hover__menu_item text__c_black hover:bg-[#e9ff70]"
+                    className="transition-all duration-200 ease-in-out hover__menu_item text__c_black hover:bg-[#d1d5db]"
                   >
                     {item.name}
                   </Link>
--- components/sitewide/EndCta.jsx
+++ components/sitewide/EndCta.jsx
@@ -10,7 +10,7 @@ const EndCta = () => {
         />
         <div className="flex items-center justify-center pm-y">
             <Link href={"/contact"}>
-                <button className="shadow rounded button__text_colour py-3 px-5 accent__c_main hover:opacity-75 duration-200 transition-all flex gap-1 items-center 2xl:text-lg">Contact Me
+                <button className="font-medium shadow rounded button__text_colour py-3 px-5 accent__c_main hover:opacity-75 duration-200 transition-all flex gap-1 items-center 2xl:text-lg">Contact Me
                     <svg
                       className="w-[17px] h-[17px] 2xl:w-[18px] 2xl:h-[18px] fill-[#2a2a2a]"
                       viewBox="0 0 15 15"
